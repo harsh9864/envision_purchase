f1f2bdc791c4f0b1a53eb5f0ca8394e94c16f178
rutikarathod1308 <rutikasanskartechnolab@gmail.com>
2025-04-25 12:25:53 +0530

    auto disqualify issues solve

--- envision/public/js/supplier_quotation.js
+++ envision/public/js/supplier_quotation.js
@@ -19,39 +19,39 @@ frappe.ui.form.on("Supplier Quotation",{
 
 frappe.ui.form.on('Supplier Quotation', {
 	after_workflow_action: (frm) => {
-    if(frm.doc.workflow_state === 'Submitted'){
-        frappe.call({
-            method: "frappe.client.get_list",
-            args: {
-                doctype: "Supplier Quotation",
-                filters: {
-                    'name': ['!=', frm.doc.name],  // Exclude the current record
-                    'custom_request_for_quotation': frm.doc.custom_request_for_quotation,
-                    'workflow_state' : "Draft"
-                }
-            },
-            callback: function(r) {
-                if (r.message && r.message.length > 0) {
-                    // Loop through the list of Supplier Quotation records
-                    var len = r.message.length;
-                    for (var i = 0; i < len; i++) {
-                        var doc = r.message[i]; // Get each Supplier Quotation document
-
-                        // Use frappe.db.set_value to update a specific field (replace 'field_name' with actual field)
-                        frappe.db.set_value("Supplier Quotation", doc.name, "workflow_state", "Disqualify")
-                            .then(r => {
-                                console.log(`Updated Supplier Quotation ${doc.name}`);
-                            })
-                            
-                    }
-                }
-            }
-        });
-    } 
-     
-    
+		if (frm.doc.workflow_state === 'Submitted') {
+			let rfq = frm.doc.custom_request_for_quotation;
+			if (rfq) {
+				frappe.call({
+					method: "frappe.client.get_list",
+					args: {
+						doctype: "Supplier Quotation",
+						filters: {
+							'custom_request_for_quotation': rfq,
+							'workflow_state': 'Draft',
+							'name': ['!=', frm.doc.name]
+						},
+						fields: ['name', 'supplier', 'workflow_state']
+					},
+					callback: function (r) {
+						if (r.message && r.message.length > 0) {
+							r.message.forEach(doc => {
+								frappe.db.set_value("Supplier Quotation", doc.name, "workflow_state", "Disqualify")
+									.then(() => {
+										console.log(`Disqualified Supplier Quotation: ${doc.name}`);
+									});
+							});
+						} else {
+							console.log("No Draft Supplier Quotations to disqualify.");
+						}
+					}
+				});
+			}
+		}
 	}
 });
+
+
 
 frappe.ui.form.on('Supplier Quotation', {
     before_workflow_action: function(frm) {
